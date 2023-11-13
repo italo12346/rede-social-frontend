4353ad39202425a960407f3b4f4ff089a44e581d
CiceroLucas <lukasferreiradesousa890@gmail.com>
2023-11-13 20:34:53 -0300

    feat: create home screen

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import Header from "./src/components/home/Header";
+import HomeScreen from "./src/screens/HomeScreen";
 
 export default function App() {
-  return <Header />;
+  return <HomeScreen />;
 }
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Header } from "../components/Header";
+
+export function HomeScreen() {
+  return <Header />;
+}
+
+export default HomeScreen;
