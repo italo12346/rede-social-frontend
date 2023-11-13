214f0d6e10e821603d80da8d8ae8678e850769ae
CiceroLucas <lukasferreiradesousa890@gmail.com>
2023-11-13 18:36:31 -0300

    initial commit

---
+++ App.tsx
@@ -0,0 +1,7 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Header from "./src/components/home/Header";
+
+export default function App() {
+  return <Header />;
+}
