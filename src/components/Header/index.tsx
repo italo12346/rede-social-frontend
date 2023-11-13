8d968285da55ca7a2c1340e15682502e6738f413
CiceroLucas <lukasferreiradesousa890@gmail.com>
2023-11-13 18:35:55 -0300

    feat: create header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { HeaderContainer, HeaderText } from "./styles";
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <HeaderText>LensLink</HeaderText>
+    </HeaderContainer>
+  );
+}
+
+export default Header;
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components/native";
+
+export const HeaderContainer = styled.View`
+  height: 50px;
+  padding: 10px;
+  margin-top: 40px;
+  background-color: #2e4374;
+  justify-content: center;
+  align-items: center;
+  border-bottom-width: 1px;
+  border-bottom-color: #ccc;
+`;
+
+export const HeaderText = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+`;
