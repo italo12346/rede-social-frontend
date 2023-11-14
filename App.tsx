d9b332baa88aa193d79ec4de78ce52b69ca8e86f
Italo souza sampaio <italo12346@gmail.com>
2023-11-14 18:31:08 -0300

    view: Criação da pagina de login
    Ainda vai ser componentizada .

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import HomeScreen from "./src/screens/HomeScreen";
+import LoginScreen from "./src/screens/Login";
+import React from "react";
 
 export default function App() {
-  return <HomeScreen />;
+  return <LoginScreen />;
 }
