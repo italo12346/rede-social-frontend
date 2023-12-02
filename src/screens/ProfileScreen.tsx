f43b96cb545d36c44d4e5d610fa877975ccbf609
CiceroLucas <lukasferreiradesousa890@gmail.com>
2023-12-02 09:36:03 -0300

    update: profile screen

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Header } from "../components/Header";
 import { StatusBar } from "react-native";
 import { Profile } from "../components/Profile";
+import Gallery from "../components/Gallery";
 //import { BottomTabNavigator } from "../components/BottomTab";
 
 export function ProfileScreen() {
@@ -10,6 +11,7 @@ export function ProfileScreen() {
       <StatusBar barStyle={"dark-content"} backgroundColor={"#2e4374"} />
       <Header title="Perfil" />
       <Profile />
+      <Gallery />
     </>
   );
 }
